7c0dc0d7385a476b484e44f5ca51583c3740d3ab
asepuehend <107599074+asepuehend@users.noreply.github.com>
2022-06-22 21:39:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ By : ${('ARNAZX#9611')}
 >  ${(`Piala By Arnazx : ${trophy}`)}  
 >  ${(`Mahkota By Arnazx : ${crown}`)}
 >  ${(`Status : Success !`)}`));
-      await sleep(6500);
+      await sleep(4500);
 
     } else if (result == 'BANNED') {
       console.log(chalk.bgRed(`Mampus Banned Makanya jangan brutal`));
